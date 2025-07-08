e0d688fdc0fd11f30bd9a6814d722e47733242f2
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-08 17:47:15 -0500

    feat: initialize project with Spring WebFlux setup
    - Added `.gitignore` and `.gitattributes` files for project configuration.
    - Configured Maven Wrapper for dependency management.
    - Created Spring Boot `WebfluxPlaygroundApplication` as the main entry point.
    - Included dependencies for Spring WebFlux, R2DBC, H2 database, and testing in `pom.xml`.

---
+++ src/test/java/org/mrpaulwoods/webfluxplayground/WebfluxPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mrpaulwoods.webfluxplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
