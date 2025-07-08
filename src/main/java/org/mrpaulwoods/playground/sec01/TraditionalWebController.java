ea03b8b21a6f0ce4ad8c434629356c2c033ad67b
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-08 18:26:41 -0500

    feat: add traditional and reactive web controllers
    - Implemented `TraditionalWebController` with synchronous REST client for fetching product data.
    - Added `ReactiveWebController` to demonstrate fetching product data using WebClient, with support for streaming responses.

---
+++ src/main/java/org/mrpaulwoods/playground/sec01/TraditionalWebController.java
@@ -0,0 +1,35 @@
+package org.mrpaulwoods.playground.sec01;
+
+import org.slf4j.Logger;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.client.JdkClientHttpRequestFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("traditional")
+public class TraditionalWebController {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(TraditionalWebController.class);
+    private final RestClient restClient = RestClient.builder()
+            .requestFactory(new JdkClientHttpRequestFactory())
+            .baseUrl("http://localhost:7070")
+            .build();
+
+    @GetMapping("products")
+    public List<Product> getProducts() {
+        var list = this.restClient.get()
+                .uri("/demo01/products")
+                .retrieve()
+                .body(new ParameterizedTypeReference<List<Product>>() {
+                });
+
+        log.info("received response: {}", list);
+        return list;
+    }
+
+}
