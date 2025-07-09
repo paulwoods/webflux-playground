d743e18347966a3f00d198c8cf2552cf65c588b6
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-08 19:33:24 -0500

    feat: enable dynamic package scanning and R2DBC repository configuration
    - Updated `application.properties` to include `sec` and SQL data location configuration.
    - Modified `WebfluxPlaygroundApplication` to enable dynamic scanning based on `sec` property and added R2DBC repository support.

--- src/main/java/org/mrpaulwoods/playground/WebfluxPlaygroundApplication.java
+++ src/main/java/org/mrpaulwoods/playground/WebfluxPlaygroundApplication.java
@@ -2,8 +2,10 @@ package org.mrpaulwoods.playground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "org.mrpaulwoods.playground.${sec}")
+@EnableR2dbcRepositories(basePackages = "org.mrpaulwoods.playground.${sec}")
 public class WebfluxPlaygroundApplication {
 
     public static void main(String[] args) {
