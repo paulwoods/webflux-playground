4be941840bce055015859d47eeba5659c8abc7c6
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-08 19:42:05 -0500

    feat: add abstract test class for sec02 package
    - Introduced `AbstractTest` with `@SpringBootTest` annotation and pre-configured properties.

---
+++ src/test/java/org/mrpaulwoods/playground/tests/sec02/AbstractTest.java
@@ -0,0 +1,8 @@
+package org.mrpaulwoods.playground.tests.sec02;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "sec=sec02")
+public abstract class AbstractTest {
+
+}
