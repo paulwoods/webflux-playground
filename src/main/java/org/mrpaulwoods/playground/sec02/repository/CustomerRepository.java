51015e0b218500d4cdf37050c1ef2d8c00b7245c
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-08 19:42:12 -0500

    feat: add Customer entity and repository for sec02
    - Introduced `Customer` entity with fields `id`, `name`, and `email`.
    - Added `CustomerRepository` interface extending `ReactiveCrudRepository`.

---
+++ src/main/java/org/mrpaulwoods/playground/sec02/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package org.mrpaulwoods.playground.sec02.repository;
+
+import org.mrpaulwoods.playground.sec02.entity.Customer;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends ReactiveCrudRepository<Customer, Integer> {
+}
