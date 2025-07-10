5bd61dd8893be0abe65442ff3be4dbc198ac080a
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-09 20:06:57 -0500

    test: update AbstractTest with additional logging configuration
    - Added `logging.level.org.springframework.r2dbc=DEBUG` to enhance R2DBC query logging in test cases.

--- src/test/java/org/mrpaulwoods/playground/tests/sec02/AbstractTest.java
+++ src/test/java/org/mrpaulwoods/playground/tests/sec02/AbstractTest.java
@@ -2,7 +2,7 @@ package org.mrpaulwoods.playground.tests.sec02;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "sec=sec02")
+@SpringBootTest(properties = {"sec=sec02", "logging.level.org.springframework.r2dbc=DEBUG"})
 public abstract class AbstractTest {
 
 }
