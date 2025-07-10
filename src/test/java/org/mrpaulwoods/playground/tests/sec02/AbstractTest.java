b2949e6e8dd80ce6ebe1b62bddd1dc1d00388052
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-09 20:07:17 -0500

    test: remove verbose logging configuration from AbstractTest
    - Commented out `logging.level.org.springframework.r2dbc=DEBUG` to simplify test configuration.

--- src/test/java/org/mrpaulwoods/playground/tests/sec02/AbstractTest.java
+++ src/test/java/org/mrpaulwoods/playground/tests/sec02/AbstractTest.java
@@ -2,7 +2,8 @@ package org.mrpaulwoods.playground.tests.sec02;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {"sec=sec02", "logging.level.org.springframework.r2dbc=DEBUG"})
+//@SpringBootTest(properties = {"sec=sec02", "logging.level.org.springframework.r2dbc=DEBUG"})
+@SpringBootTest(properties = {"sec=sec02"})
 public abstract class AbstractTest {
 
 }
