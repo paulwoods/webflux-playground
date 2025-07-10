96334d7389adb2d47a8911f21f5644d9e2a88a73
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-09 20:24:30 -0500

    feat: support pagination in ProductRepository with tests
    - Added `findBy(Pageable pageable)` method to `ProductRepository` for paginated queries.
    - Implemented integration tests in `Lec02ProductRepositoryTests` to validate pagination functionality.

--- src/main/java/org/mrpaulwoods/playground/sec02/repository/ProductRepository.java
+++ src/main/java/org/mrpaulwoods/playground/sec02/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package org.mrpaulwoods.playground.sec02.repository;
 
 import org.mrpaulwoods.playground.sec02.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
@@ -10,4 +11,7 @@ public interface ProductRepository extends ReactiveCrudRepository<Product, Integ
     Flux<Product> findByDescription(String ipad);
 
     Flux<Product> findAllByPriceBetween(Integer lower, Integer upper);
+
+    Flux<Product> findBy(Pageable pageable);
+
 }
--- src/test/java/org/mrpaulwoods/playground/tests/sec02/Lec02ProductRepositoryTests.java
+++ src/test/java/org/mrpaulwoods/playground/tests/sec02/Lec02ProductRepositoryTests.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.mrpaulwoods.playground.sec02.repository.ProductRepository;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import reactor.test.StepVerifier;
 
 public class Lec02ProductRepositoryTests extends AbstractTest {
@@ -45,7 +47,7 @@ public class Lec02ProductRepositoryTests extends AbstractTest {
     }
 
     @Test
-    public void findAlLByPriceRange() {
+    public void findAllByPriceRange() {
         this.repository.findAllByPriceBetween(200, 300)
                 .doOnNext(p -> log.info("{}", p))
                 .as(StepVerifier::create)
@@ -56,4 +58,42 @@ public class Lec02ProductRepositoryTests extends AbstractTest {
                 .verify();
     }
 
+    @Test
+    public void pageable() {
+        this.repository.findBy(PageRequest.of(0, 3).withSort(Sort.by("price").ascending()))
+                .doOnNext(p -> log.info("{}", p))
+                .as(StepVerifier::create)
+                .assertNext(p -> Assertions.assertEquals(200, p.getPrice()))
+                .assertNext(p -> Assertions.assertEquals(250, p.getPrice()))
+                .assertNext(p -> Assertions.assertEquals(300, p.getPrice()))
+                .expectComplete()
+                .verify();
+    }
+
+    @Test
+    public void pageable2ndPage() {
+        this.repository.findBy(PageRequest.of(1, 3).withSort(Sort.by("price").ascending()))
+                .doOnNext(p -> log.info("{}", p))
+                .as(StepVerifier::create)
+                .assertNext(p -> Assertions.assertEquals(400, p.getPrice()))
+                .assertNext(p -> Assertions.assertEquals(750, p.getPrice()))
+                .assertNext(p -> Assertions.assertEquals(800, p.getPrice()))
+                .expectComplete()
+                .verify();
+    }
+
 }
+/*
+INSERT INTO product(description, price)
+VALUES ('iphone 20', 1000),
+       ('iphone 18', 750),
+       ('ipad', 800),
+       ('mac pro', 3000),
+       ('apple watch', 400),
+       ('macbook air', 1200),
+       ('airpods pro', 250),
+       ('imac', 2000),
+       ('apple tv', 200),
+       ('homepod', 300);
+
+ */
