ef1d66cfe8853633b85e3e6bfbd63a83d9a59d16
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-09 20:50:55 -0500

    test: add integration test for fetching order details by product
    - Created `Lec04DatabaseClientTest` to validate SQL query for retrieving order details by product description.
    - Utilized R2DBC `DatabaseClient` and `OrderDetails` DTO for mapping query results.
    - Added assertions to ensure correct data retrieval and ordering.

---
+++ src/test/java/org/mrpaulwoods/playground/tests/sec02/Lec04DatabaseClientTest.java
@@ -0,0 +1,48 @@
+package org.mrpaulwoods.playground.tests.sec02;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mrpaulwoods.playground.sec02.dto.OrderDetails;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.r2dbc.core.DatabaseClient;
+import reactor.test.StepVerifier;
+
+public class Lec04DatabaseClientTest extends AbstractTest {
+
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(Lec04DatabaseClientTest.class);
+
+    @Autowired
+    private DatabaseClient client;
+
+    @Test
+    public void orderDetailsByProduct() {
+        var query = """
+                    SELECT
+                    co.order_id,
+                    c.name AS customer_name,
+                    p.description AS product_name,
+                    co.amount,
+                    co.order_date
+                FROM
+                    customer c
+                INNER JOIN customer_order co ON c.id = co.customer_id
+                INNER JOIN product p ON p.id = co.product_id
+                WHERE
+                    p.description = :description
+                ORDER BY co.amount DESC
+                """;
+
+        this.client.sql(query)
+                .bind("description", "iphone 20")
+                .mapProperties(OrderDetails.class)
+                .all()
+                .doOnNext(dto -> log.info("{}", dto))
+                .as(StepVerifier::create)
+                .assertNext(dto -> Assertions.assertEquals(975, dto.amount()))
+                .assertNext(dto -> Assertions.assertEquals(950, dto.amount()))
+                .expectComplete()
+                .verify();
+    }
+
+}
