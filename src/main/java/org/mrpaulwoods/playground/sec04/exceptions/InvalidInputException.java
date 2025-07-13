592f32edf4790133cfdd5444241ebc8f059ce93e
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-13 11:26:20 -0500

    feat: add custom exceptions for enhanced error handling
    - Introduced `CustomerNotFoundException` and `InvalidInputException` to represent specific error cases.
    - Created `ApplicationExceptions` utility class to simplify error propagation using Reactor's `Mono.error`.

---
+++ src/main/java/org/mrpaulwoods/playground/sec04/exceptions/InvalidInputException.java
@@ -0,0 +1,8 @@
+package org.mrpaulwoods.playground.sec04.exceptions;
+
+public class InvalidInputException extends RuntimeException {
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
