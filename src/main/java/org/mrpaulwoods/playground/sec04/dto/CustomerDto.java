7de7d8ad289eb90161dbea47e6fdfb3b700e97d4
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-13 11:25:35 -0500

    feat: copied sec03 to sec04

---
+++ src/main/java/org/mrpaulwoods/playground/sec04/dto/CustomerDto.java
@@ -0,0 +1,8 @@
+package org.mrpaulwoods.playground.sec04.dto;
+
+public record CustomerDto(
+        Integer id,
+        String name,
+        String email
+) {
+}
---
+++ src/main/java/org/mrpaulwoods/playground/sec04/mapper/EntityDtoMapper.java
@@ -0,0 +1,23 @@
+package org.mrpaulwoods.playground.sec04.mapper;
+
+import org.mrpaulwoods.playground.sec04.dto.CustomerDto;
+import org.mrpaulwoods.playground.sec04.entity.Customer;
+
+public class EntityDtoMapper {
+
+    public static Customer toEntity(CustomerDto dto) {
+        var customer = new Customer();
+        customer.setId(dto.id());
+        customer.setName(dto.name());
+        customer.setEmail(dto.email());
+        return customer;
+    }
+
+    public static CustomerDto toDto(Customer entity) {
+        return new CustomerDto(
+                entity.getId(),
+                entity.getName(),
+                entity.getEmail()
+        );
+    }
+}
