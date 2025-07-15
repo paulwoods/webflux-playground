bc5d6791689cdc7a4392bd132c201aa58fcd9f2d
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-15 08:24:59 -0500

    feat: starting section 05

---
+++ src/main/java/org/mrpaulwoods/playground/sec05/dto/CustomerDto.java
@@ -0,0 +1,8 @@
+package org.mrpaulwoods.playground.sec05.dto;
+
+public record CustomerDto(
+        Integer id,
+        String name,
+        String email
+) {
+}
---
+++ src/main/java/org/mrpaulwoods/playground/sec05/service/CustomerService.java
@@ -0,0 +1,55 @@
+package org.mrpaulwoods.playground.sec05.service;
+
+import org.mrpaulwoods.playground.sec05.dto.CustomerDto;
+import org.mrpaulwoods.playground.sec05.mapper.EntityDtoMapper;
+import org.mrpaulwoods.playground.sec05.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public Flux<CustomerDto> getAllCustomers() {
+        return this.customerRepository.findAll()
+                .map(EntityDtoMapper::toDto);
+    }
+
+    public Mono<List<CustomerDto>> getAllCustomers(Integer page, Integer size) {
+        return this.customerRepository.findBy(PageRequest.of(page - 1, size))
+                .map(EntityDtoMapper::toDto)
+                .collectList();
+    }
+
+    public Mono<CustomerDto> getCustomerById(Integer id) {
+        return this.customerRepository.findById(id)
+                .map(EntityDtoMapper::toDto);
+    }
+
+    public Mono<CustomerDto> saveCustomer(Mono<CustomerDto> mono) {
+        return mono.map(EntityDtoMapper::toEntity)
+                .flatMap(this.customerRepository::save)
+                .map(EntityDtoMapper::toDto);
+    }
+
+    public Mono<CustomerDto> updateCustomer(Integer id, Mono<CustomerDto> mono) {
+        return this.customerRepository.findById(id)
+                .flatMap(entity -> mono)
+                .map(EntityDtoMapper::toEntity)
+                .doOnNext(c -> c.setId(id))
+                .flatMap(this.customerRepository::save)
+                .map(EntityDtoMapper::toDto);
+    }
+
+    public Mono<Boolean> deleteCustomerById(Integer id) {
+        return this.customerRepository.deleteCustomerById(id);
+    }
+
+}
