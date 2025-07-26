58719c1b3f27595b926a6ebc9c3964523f937aad
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-25 20:54:08 -0500

    test: add `Lec09ExchangeFilterTest` to validate WebClient exchange filters
    - Introduced a test to verify WebClient exchange filter functionality for token generation.
    - Validated API responses using `StepVerifier` with reactive programming.
    - Logged dynamically generated bearer tokens for debugging purposes.

---
+++ src/test/java/org/mrpaulwoods/playground/tests/sec07/Lec09ExchangeFilterTest.java
@@ -0,0 +1,45 @@
+package org.mrpaulwoods.playground.tests.sec07;
+
+import org.junit.jupiter.api.Test;
+import org.mrpaulwoods.playground.sec02.entity.Product;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.test.StepVerifier;
+
+import java.util.UUID;
+
+public class Lec09ExchangeFilterTest extends AbstractWebClient {
+
+    public static final Logger log = LoggerFactory.getLogger(Lec09ExchangeFilterTest.class);
+
+    private final WebClient client = createWebClient(b -> b.filter(tokenGenerator()));
+
+    @Test
+    public void exchangeFilter() {
+        for (int i = 0; i < 5; i++) {
+
+            this.client.get()
+                    .uri("/lec09/product/{id}", 1)
+                    .retrieve()
+                    .bodyToMono(Product.class)
+                    .doOnNext(print())
+                    .then()
+                    .as(StepVerifier::create)
+                    .expectComplete()
+                    .verify();
+        }
+    }
+
+    private ExchangeFilterFunction tokenGenerator() {
+        return (request, next) -> {
+            var token = UUID.randomUUID().toString().replace("-", "");
+            log.info("generated token: {}", token);
+            var modifiedRequest = ClientRequest.from(request).headers(h -> h.setBearerAuth(token)).build();
+            return next.exchange(modifiedRequest);
+        };
+    }
+
+}
