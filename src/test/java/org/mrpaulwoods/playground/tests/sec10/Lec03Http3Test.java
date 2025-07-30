3dcfd2ef3ab93b03387ff45e28534afe35f5d57c
mrpaulwoods <mr.paul.woods@gmail.com>
2025-07-29 20:33:47 -0500

    test: add `Lec03Http3Test` for testing HTTP/3 concurrent requests
    - Introduced `Lec03Http3Test` to evaluate WebClient behavior with HTTP/3 and increased load.
    - Configured custom connection pool with `ReactorClientHttpConnector` supporting HTTP/3.
    - Added `concurrentRequests` test to verify handling of 3,000 concurrent requests.
    - Updated `application.properties` to enable HTTP/2 configuration.

---
+++ src/test/java/org/mrpaulwoods/playground/tests/sec10/Lec03Http3Test.java
@@ -0,0 +1,60 @@
+package org.mrpaulwoods.playground.tests.sec10;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mrpaulwoods.playground.tests.sec10.dto.Product;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.netty.http.HttpProtocol;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.resources.ConnectionProvider;
+import reactor.test.StepVerifier;
+
+public class Lec03Http3Test extends AbstractWebClient {
+
+    private final WebClient client = createWebClient(b -> {
+
+        // change the connection pool to poolSize, and change the queue size to poolSize * 5
+
+        var poolSize = 1;
+
+        var provider = ConnectionProvider.builder("vins")
+                .lifo()
+                .maxConnections(poolSize)
+                .build();
+
+        var httpClient = HttpClient.create(provider)
+                .protocol(HttpProtocol.H2C)
+                .compress(true)
+                .keepAlive(true);
+
+        b.clientConnector(new ReactorClientHttpConnector(httpClient));
+    });
+
+    private Mono<Product> getProduct(int id) {
+        return client.get()
+                .uri("/product/{id}", id)
+                .retrieve()
+                .bodyToMono(Product.class);
+    }
+
+    @Test
+    public void concurrentRequests() {
+        // flat map has a default size of 256
+        // webclient has a default size of 500
+
+        var max = 3000;
+        Flux.range(1, max)
+                .flatMap(this::getProduct, max)
+                .collectList()
+                .as(StepVerifier::create)
+                .assertNext(list -> Assertions.assertEquals(max, list.size()))
+                .expectComplete()
+                .verify();
+    }
+
+}
+
+// watch 'netstat -an| grep -w 127.0.0.1.7070'
