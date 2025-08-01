8d0d24444fc5c866ed4885a66fc6c3e5a3ed5268
mrpaulwoods <mr.paul.woods@gmail.com>
2025-08-01 18:39:35 -0500

    feat: initialize `aggregator-service` project
    - Added a new Spring Boot project for `aggregator-service`.
    - Included Maven Wrapper, `.gitattributes`, and `.gitignore` files.
    - Configured `pom.xml` with dependencies for WebFlux and Spring Boot.
    - Added application entry point and placeholder test class.

---
+++ aggregator-service/src/test/java/org/mrpaulwoods/aggregatorservice/AggregatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mrpaulwoods.aggregatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AggregatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
