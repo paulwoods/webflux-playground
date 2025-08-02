d5db901f0b3379e5032a8a9ffd454a531339b05b
mrpaulwoods <mr.paul.woods@gmail.com>
2025-08-02 17:06:53 -0500

    feat: enhance `CustomerInformationTest` with error handling and validation
    - Added `customerNotFound` test case to validate 404 response handling.
    - Refactored `CustomerInformationTest` with reusable `mockCustomerInformation` and `getCustomerInformation` helpers.
    - Included mock response `customer-information-404.json` for 404 error scenario.

--- aggregator-service/src/test/java/org/mrpaulwoods/aggregator/tests/CustomerInformationTest.java
+++ aggregator-service/src/test/java/org/mrpaulwoods/aggregator/tests/CustomerInformationTest.java
@@ -5,6 +5,8 @@ import org.mockserver.model.HttpRequest;
 import org.mockserver.model.HttpResponse;
 import org.mockserver.model.MediaType;
 import org.slf4j.Logger;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.Objects;
 
@@ -16,20 +18,50 @@ public class CustomerInformationTest extends AbstractIntegrationTest {
 
     @Test
     public void customerInformation() {
-        // mock customer service
-        var responseBody = this.resourceToString("customer-service/customer-information-200.json");
+        // when
+        mockCustomerInformation("customer-service/customer-information-200.json", 200);
+
+        // then:
+        getCustomerInformation(HttpStatus.OK)
+                .jsonPath("$.id").isEqualTo(1)
+                .jsonPath("$.name").isEqualTo("Sam")
+                .jsonPath("$.balance").isEqualTo(10000)
+                .jsonPath("$.holdings").isNotEmpty();
+    }
+
+
+    @Test
+    public void customerNotFound() {
+        // given:
+        mockCustomerInformation("customer-service/customer-information-404.json", 404);
+
+        // then:
+        getCustomerInformation(HttpStatus.NOT_FOUND)
+                .jsonPath("$.detail").isEqualTo("Customer [id=1] not found.")
+                .jsonPath("$.title").isNotEmpty();
+    }
+
+    // mock customer service
+    private void mockCustomerInformation(String path, int responseCode) {
+        var responseBody = this.resourceToString(path);
         mockServerClient
                 .when(HttpRequest.request("/customers/1"))
                 .respond(HttpResponse.response(responseBody)
-                        .withStatusCode(200)
+                        .withStatusCode(responseCode)
                         .withContentType(MediaType.APPLICATION_JSON)
                 );
 
-        this.client.get()
+    }
+
+    private WebTestClient.BodyContentSpec getCustomerInformation(HttpStatus expectedStatus) {
+
+        return this.client.get()
                 .uri("/customers/1")
                 .exchange()
+                .expectStatus().isEqualTo(expectedStatus)
                 .expectBody()
                 .consumeWith(e -> log.info("####### {}", new String(Objects.requireNonNull(e.getResponseBody()))));
+
     }
 
 }
