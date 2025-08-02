346c848f35d6aa95d896f2b0ba456824455c852c
mrpaulwoods <mr.paul.woods@gmail.com>
2025-08-01 19:09:14 -0500

    feat: enhance `StockServiceClient` with retry logic and caching for price updates
    - Added retry mechanism with fixed delay to handle streaming failures.
    - Implemented caching for price update streams to optimize performance.
    - Introduced lazy initialization for `priceUpdateStream` to ensure efficient stream reuse.

--- aggregator-service/src/main/java/org/mrpaulwoods/aggregator/client/StockServiceClient.java
+++ aggregator-service/src/main/java/org/mrpaulwoods/aggregator/client/StockServiceClient.java
@@ -8,12 +8,16 @@ import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
+import java.util.Objects;
 
 public class StockServiceClient {
 
     private static final Logger log = LoggerFactory.getLogger(StockServiceClient.class);
-
-    public final WebClient client;
+    private final WebClient client;
+    private Flux<PriceUpdate> flux;
 
     public StockServiceClient(WebClient client) {
         this.client = client;
@@ -26,12 +30,27 @@ public class StockServiceClient {
                 .bodyToMono(StockPriceResponse.class);
     }
 
-    public Flux<PriceUpdate> getPriceUpdate() {
+    public Flux<PriceUpdate> priceUpdateStream() {
+        if(Objects.isNull(this.flux)) {
+            this.flux = this.getPriceUpdate();
+        }
+        return this.flux;
+    }
+
+    private Flux<PriceUpdate> getPriceUpdate() {
         return this.client.get()
                 .uri("/stock/price-stream")
                 .accept(MediaType.APPLICATION_NDJSON)
                 .retrieve()
-                .bodyToFlux(PriceUpdate.class);
+                .bodyToFlux(PriceUpdate.class)
+                .retryWhen(retry())
+                .cache(1);
+    }
+
+    private Retry retry() {
+        return Retry.fixedDelay(100, Duration.ofSeconds(1))
+                .doBeforeRetry(rs -> log.error("stock service price stream call failed. retrying: {}", rs.failure().getMessage()));
+
     }
 
 }
