6c973c11edc55034d63313a95f7902f14ad5dee9
mrpaulwoods <mr.paul.woods@gmail.com>
2025-08-01 19:00:31 -0500

    feat: add `RequestValidator` and enhance exception handling in `aggregator-service`
    - Introduced `RequestValidator` for validating `TradeRequest` inputs including ticker, trade action, and quantity.
    - Added `InvalidTradeRequestException` for handling trade-related validation errors.
    - Extended `ApplicationExceptions` with methods for detailed trade request validation scenarios.

--- aggregator-service/src/main/java/org/mrpaulwoods/aggregator/exceptions/ApplicationExceptions.java
+++ aggregator-service/src/main/java/org/mrpaulwoods/aggregator/exceptions/ApplicationExceptions.java
@@ -8,4 +8,20 @@ public class ApplicationExceptions {
         return Mono.error(new CustomerNotFoundException(customerId));
     }
 
+    public static <T> Mono<T> invalidTradeRequest(String message) {
+        return Mono.error(new InvalidTradeRequestException(message));
+    }
+
+    public static <T> Mono<T> missingTicker() {
+        return Mono.error(new InvalidTradeRequestException("Ticker is required"));
+    }
+
+    public static <T> Mono<T> missingTradeAction() {
+        return Mono.error(new InvalidTradeRequestException("Trade action required"));
+    }
+
+    public static <T> Mono<T> invalidQuantity() {
+        return Mono.error(new InvalidTradeRequestException("Quantity should be greater than zero"));
+    }
+
 }
---
+++ aggregator-service/src/main/java/org/mrpaulwoods/aggregator/exceptions/InvalidTradeRequestException.java
@@ -0,0 +1,9 @@
+package org.mrpaulwoods.aggregator.exceptions;
+
+public class InvalidTradeRequestException extends RuntimeException{
+
+    public InvalidTradeRequestException(String message) {
+        super(message);
+    }
+
+}
